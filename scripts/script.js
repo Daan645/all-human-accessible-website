28a2a82d0d7d6224fc5efcf15474173459cbd14d
J3SS3HVA <jesse.leeuwerink@hva.nl>
2023-10-31 13:11:15 +0100

    jesses changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,41 @@
 function leesmeer1() {
     var line = document.getElementById("line");
-    var moreText = document.getElementById("meerWelkom");
-    var btnText = document.getElementById("meer");
+    var moreText = document.getElementById("moreWelcome");
+    var btnText = document.getElementById("more");
+    // var textarea = document.querySelector('.generaltext');
   
     if (line.style.display === "none") {
       line.style.display = "inline";
       btnText.innerHTML = "Read more"; 
       moreText.style.display = "none";
+      // textarea.style.height = "";
+      
+      
     } else {
       line.style.display = "none";
       btnText.innerHTML = "Read less"; 
       moreText.style.display = "inline";
+      // textarea.style.height = "600px";
+    }
+  }
+
+  function leesmeer2() {
+    var line2 = document.getElementById("line2");
+    var moreText2 = document.getElementById("moreAbout");
+    var btnText2 = document.getElementById("more2");
+    // var textarea = document.querySelector('.generaltext');
+  
+    if (line2.style.display === "none") {
+      line2.style.display = "inline";
+      btnText2.innerHTML = "Read more"; 
+      moreText2.style.display = "none";
+      // textarea.style.height = "";
+      
+      
+    } else {
+      line2.style.display = "none";
+      btnText2.innerHTML = "Read less"; 
+      moreText2.style.display = "inline";
+      // textarea.style.height = "600px";
     }
   }
