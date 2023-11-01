93d7a96bdf5c238ca36317c7c0d521838187029d
RubenErhardt <rubengamebattles@outlook.com>
2023-11-01 10:34:27 +0100

    img fix

--- script.js
+++ script.js
@@ -12,4 +12,24 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+function toggleReview(element) {
+    element.classList.toggle("active");
+}
+
+const carousel = document.querySelector('.carousel');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const articleWidth = 300; // Breedte van elk artikel (aanpassen aan je eigen stijl)
+
+let currentPosition = 0;
+
+function moveCarousel(direction) {
+  const maxPosition = Math.max(0, carousel.scrollWidth - carousel.clientWidth);
+  currentPosition += direction * articleWidth;
+  currentPosition = Math.min(maxPosition, Math.max(0, currentPosition));
+  carousel.style.transform = `translateX(-${currentPosition}px)`;
+}
+
+prevButton.addEventListener('click', () => moveCarousel(-1));
+nextButton.addEventListener('click', () => moveCarousel(1));
 
