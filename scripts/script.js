ee19d2d8b56c1ae366616c348127ee7617155e55
Daan645 <daan@videotechniek.nl>
2023-11-01 20:13:33 +0100

    added timeline
    timeline

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,18 @@ function leesmeer1() {
       // textarea.style.height = "600px";
     }
   }
+
+  //timeline
+// Voeg een klikgebeurtenis toe aan de cirkels (containers)
+const containers = document.querySelectorAll('.timelinecontainer');
+containers.forEach(container => {
+    container.addEventListener('click', () => {
+        // Verander de zichtbaarheid van de bijbehorende content
+        const content = container.querySelector('.content');
+        if (content.style.visibility === 'hidden' || content.style.visibility === '') {
+            content.style.visibility = 'visible';
+        } else {
+            content.style.visibility = 'hidden';
+        }
+    });
+});
